87ee606827cf16bb150bbf55c5c17f7b6052cfea
daanish93 <daanishrehman93@gmail.com>
2019-04-12 03:36:06 -0400

    added some comments

--- COMP345/StepSingleton.cpp
+++ COMP345/StepSingleton.cpp
@@ -6,6 +6,8 @@ using std::endl;
 
 StepSingleton::StepSingleton()
 {
+	// step 1 and lazy loading 
+	// only when step incease to 2 an object was created
 	step = 1;
 	instance = nullptr;
 }
@@ -27,14 +29,13 @@ void StepSingleton::setStep(int stepNumber)
 	// only increment the step up to 3
 	if (stepNumber >= 1 && step <= 3)
 		step = stepNumber;
-	//cout << "-----------------------------------------------" << endl;
-	cout << "Step was set to " << step << "." << endl;
-	cout << "-----------------------------------------------" << endl;
 }
 
 
 StepSingleton * StepSingleton::getInstance()
 {
+	// if the instance is not already created make one and then if it doesn't exist return the current instance
+	// it'll guarentee only 1 intance of the singleton
 	if (instance == nullptr)
 		instance = new StepSingleton();
 	return instance;
